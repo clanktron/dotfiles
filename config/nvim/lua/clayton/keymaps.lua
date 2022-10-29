cbdb74fe4b0f767d62fbb7851a0cbda3c5a21ef8
clayton <crdcastro5@gmail.com>
2022-10-29 01:21:59 -0700

    change vim tab keymaps from h and l to j and k

--- config/nvim/lua/clayton/keymaps.lua
+++ config/nvim/lua/clayton/keymaps.lua
@@ -2,5 +2,5 @@ local keymap = vim.keymap
 
 -- tab shortcuts
 keymap.set("n", "<C-t>", "<Esc>:Texplore<CR>")
-keymap.set("n", "<C-l>", "<Esc>:tabnext<CR>")
-keymap.set("n", "<C-h>", "<Esc>:tabprevious<CR>")
+keymap.set("n", "<C-j>", "<Esc>gt")
+keymap.set("n", "<C-k>", "<Esc>gT")
