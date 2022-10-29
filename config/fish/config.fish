7449eb4b650c5beae4ba74586104077ff0a171ac
clayton <crdcastro5@gmail.com>
2022-10-28 21:56:01 -0700

    alacritty and fish update

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -2,27 +2,32 @@
 set --export --prepend PATH "/Users/clayton/.rd/bin"
 ### MANAGED BY RANCHER DESKTOP END (DO NOT EDIT)
 
-# Set Defaults
+# Colorscheme
+set COLOR nightfox 
+source (dirname (status --current-filename))/colors/$COLOR.fish
+
+# Fish defaults
 set fish_greeting ""
-set -gx EDITOR vim
+fish_vi_key_bindings
+
+# Environment vars
+set -gx EDITOR nvim
 #set -gx EDITOR 'code --wait'
 set -gx HOMEBREW_NO_ENV_HINTS 1
+set -gx GOPATH '~/Developer/go/'
+set -gx PATH $PATH:$GOPATH/bin
 
-# Theme
-set -g theme_color_scheme terminal-dark
+# Prompt
 set -g fish_prompt_pwd_dir_length 1
 set -g theme_display_user yes
 set -g theme_hide_hostname no
 set -g theme_hostname always
 
-# Aliases
+# General aliases
 alias ip "ifconfig | grep 'broadcast' | awk '{print \$2}'"
-alias sleep1 "sudo pmset disablesleep -1"
-alias sleep0 "sudo pmset disablesleep -0"
-alias dns "sudo dscacheutil -flushcache;sudo killall -HUP mDNSResponder"
 alias restart 'sudo shutdown -r now'
 alias la 'ls -a'
-alias ll 'ls -lha'
+alias ll 'ls -lh'
 alias k kubectl
 alias kd 'kubectl describe'
 alias kg 'kubectl get'
@@ -31,8 +36,8 @@ alias kc 'kubectx'
 alias docker 'nerdctl'
 alias ucla-vpn 'sudo openconnect --protocol=anyconnect https://ssl.vpn.ucla.edu'
 
-# Functions (might move these to dedicated folder or file)
-function kn 
+# Functions
+function kn
     kubectl config set-context --current --namespace="$argv" && kubectl config view --minify | grep namespace:
 end
 
@@ -49,8 +54,8 @@ function ....
      cd ../../..
 end
 
-function search 
-    open -a firefox http://www.google.com/search"?q=$argv"
+function search
+    open -a firefox https://google.com/search"?q=$argv"
 end
 
 function sudo 
@@ -70,9 +75,9 @@ end
 # Source additional config based on OS
 switch (uname)
   case Darwin
-    source (dirname (status --current-filename))/config-osx.fish
+    source (dirname (status --current-filename))/os/mac.fish
   case Linux
-    source (dirname (status --current-filename))/config-linux.fish
+    source (dirname (status --current-filename))/os/linux.fish
   case '*'
-    source (dirname (status --current-filename))/config-windows.fish
+    source (dirname (status --current-filename))/os/windows.fish
 end
---
+++ config/fish/os/mac.fish
@@ -0,0 +1,4 @@
+# Aliases
+alias sleep1 "sudo pmset disablesleep -1"
+alias sleep0 "sudo pmset disablesleep -0"
+alias dns "sudo dscacheutil -flushcache;sudo killall -HUP mDNSResponder"
