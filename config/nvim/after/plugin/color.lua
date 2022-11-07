1470eb40d64f1bbe63dd768346339a3fe57f4226
clayton <crdcastro5@gmail.com>
2022-11-07 01:12:19 -0800

    neovim touchups

--- config/nvim/after/plugin/color.lua
+++ config/nvim/after/plugin/color.lua
@@ -1,21 +1,13 @@
--- general
--- vim.opt.background = 'transparent'
-
 -- tokyonight
 require("tokyonight").setup({
-  -- your configuration comes here
-  -- or leave it empty to use the default settings
-  style = "storm", -- The theme comes in three styles, `storm`, `moon`, a darker variant `night` and `day`
-  transparent = true, -- Enable this to disable setting the background color
-  terminal_colors = true, -- Configure the colors used when opening a `:terminal` in Neovim
+  style = "storm",
+  transparent = true,
+  terminal_colors = true,
   styles = {
-    -- Background styles. Can be "dark", "transparent" or "normal"
-    sidebars = "transparent", -- style for sidebars, see below
-    floats = "transparent", -- style for floating windows
+    sidebars = "transparent",
+    floats = "transparent",
   },
 })
--- vim.g.tokyonight_transparent_sidebar = true
--- vim.g.tokyonight_transparent = true
 
 -- nightfox (needs to be configured to work with treesitter?)
 
