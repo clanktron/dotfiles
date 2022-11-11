667e9afe2a45d3c52ef862de041cd92debaa9218
clayton <crdcastro5@gmail.com>
2022-11-10 16:33:38 -0800

    added path and no more detaching

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -6,6 +6,7 @@ set -gx HOMEBREW_NO_ENV_HINTS 1
 set -gx PATH /usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin
 set -gx GOPATH ~/Developer/go
 set -px PATH $GOPATH/bin
+set -px PATH ~/.local/bin
 
 # Fish defaults
 set fish_greeting ""
