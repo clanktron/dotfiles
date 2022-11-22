e965a23d57cc0ee28e137d49706c9c4c40734bf7
clayton <crdcastro5@gmail.com>
2022-11-21 22:29:19 -0800

    local shmocal

--- config/nvim/lua/clayton/keymaps.lua
+++ config/nvim/lua/clayton/keymaps.lua
@@ -1,28 +1,26 @@
-local keymap = vim.keymap
-
 -- set custom leader key
 vim.g.mapleader = " "
 
 -- tab shortcuts
-keymap.set("n", "<leader>t", "<Esc>:tabnew<CR>")
-keymap.set("n", "<leader>l", "<Esc>gt")
-keymap.set("n", "<leader>h", "<Esc>gT")
+vim.keymap.set("n", "<leader>t", "<Esc>:tabnew<CR>")
+vim.keymap.set("n", "<leader>l", "<Esc>gt")
+vim.keymap.set("n", "<leader>h", "<Esc>gT")
 -- keymap.set("n", "<C-k>", "<Esc>gt")
 -- keymap.set("n", "<C-j>", "<Esc>gT")
 
 -- vim-maximizer
-keymap.set("n", "<leader>m", ":MaximizerToggle<CR>")
+vim.keymap.set("n", "<leader>m", ":MaximizerToggle<CR>")
 
 -- window splits
-keymap.set("n", "<leader>v", "<C-w>v")
-keymap.set("n", "<leader>o", "<C-w>s")
+vim.keymap.set("n", "<leader>v", "<C-w>v")
+vim.keymap.set("n", "<leader>o", "<C-w>s")
 
 -- open directory
-keymap.set("n", "<leader>e", ":NvimTreeToggle<CR>")
+vim.keymap.set("n", "<leader>e", ":NvimTreeToggle<CR>")
 -- keymap.set("n", "<leader>e", "<Esc>:E.<CR>")
 
 -- fast quit
-keymap.set("n", "<leader>q", "<Esc>:q<CR>")
+vim.keymap.set("n", "<leader>q", "<Esc>:q<CR>")
 
 -- telescope
 local builtin = require('telescope.builtin')
