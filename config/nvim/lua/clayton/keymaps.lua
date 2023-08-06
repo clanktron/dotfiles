8a6364b8d064f545b27e2c5394a360d2f838cbdc
clayton <crdcastro5@gmail.com>
2023-08-06 02:26:08 -0700

    update: minor cleanup

--- config/nvim/lua/clayton/keymaps.lua
+++ config/nvim/lua/clayton/keymaps.lua
@@ -16,32 +16,31 @@ vim.keymap.set({'n','v','i'}, "<Esc>", '<Esc><Esc>')
 vim.keymap.set('x', '<leader>p', "\"_dP")
 
 -- tab shortcuts
-vim.keymap.set("n", "<leader>t", "<Esc>:tabnew<CR>")
+vim.keymap.set("n", "<leader>t", "<cmd>tabnew<cr>")
 vim.keymap.set("n", "<leader>l", "<Esc>gt")
 vim.keymap.set("n", "<leader>h", "<Esc>gT")
 -- keymap.set("n", "<C-k>", "<Esc>gt")
 -- keymap.set("n", "<C-j>", "<Esc>gT")
 
 -- show path
-vim.keymap.set("n", ":path", ":echo expand('%:p')")
-
--- vim-maximizer
-vim.keymap.set("n", "<leader>m", ":MaximizerToggle<CR>")
+vim.api.nvim_create_user_command("Path", "echo expand('%:p')", {})
 
 -- window splits
 vim.keymap.set("n", "<leader>v", "<C-w>v")
 vim.keymap.set("n", "<leader>o", "<C-w>s")
 
--- no more case sensitive saves
-vim.keymap.set("n", ":W", ":w")
-vim.keymap.set("n", ":Wq", ":wq")
+-- no more case sensitive saves or quits
+vim.api.nvim_create_user_command("W", "w", {})
+vim.api.nvim_create_user_command("Q", "q", {})
+vim.api.nvim_create_user_command("Wq", "wq", {})
+vim.api.nvim_create_user_command("WQ", "wq", {})
 
 -- open directory with netrw
 -- vim.keymap.set("n", "<leader>e", "<Esc>:E.<CR>")
 
 -- fast quit and save
-vim.keymap.set("n", "<leader>q", "<Esc>:q<CR>")
-vim.keymap.set("n", "<leader>w", "<Esc>:w<CR>")
+vim.keymap.set("n", "<leader>q", "<cmd>q<cr>")
+vim.keymap.set("n", "<leader>w", "<cmd>w<cr>")
 
 -- -- popup navigation
 -- vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
