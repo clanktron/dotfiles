9549ec3f00db8c3719191b3b81602bf61e61bd4f
clayton <crdcastro5@gmail.com>
2023-08-08 18:40:08 -0700

    testing: trying out telescope file browser

--- config/nvim/lua/clayton/plugin/telescope.lua
+++ config/nvim/lua/clayton/plugin/telescope.lua
@@ -24,12 +24,13 @@ telescope.setup ({
 
 telescope.load_extension('fzf')
 telescope.load_extension('dap')
+telescope.load_extension('file_browser')
 
 -- telescope
+-- vim.keymap.set('n', '<leader>e', "<cmd>Telescope file_browser<cr>", {})
 vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
 vim.keymap.set('n', '<leader>fk', builtin.keymaps, {})
 vim.keymap.set('n', '<leader>fs', builtin.live_grep, {})
-vim.keymap.set('n', '<leader>fc', builtin.grep_string, {})
-vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
-vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
-
+-- vim.keymap.set('n', '<leader>fc', builtin.grep_string, {})
+-- vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+-- vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
