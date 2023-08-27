0669f2193e22b00f3c823a01a5fe9d0bc2dbfae4
clayton <crdcastro5@gmail.com>
2023-08-27 02:14:33 -0700

    feat(kiwi): add new optional values to opts
    corresponding to PR in kiwi.nvim

--- config/nvim/lua/clayton/plugins/kiwi.lua
+++ config/nvim/lua/clayton/plugins/kiwi.lua
@@ -1,29 +1,35 @@
 return {
     -- lightweight vimwiki
-    'serenevoid/kiwi.nvim',
+    dir = '/Users/clayton/Developer/repos/vendor/kiwi.nvim',
     lazy = true,
     dependencies = {
         'nvim-lua/plenary.nvim',
     },
     opts = {
         {
-          name = "wiki",
-          path = os.getenv("WIKI")
+            name = "wiki",
+            path = os.getenv("WIKI"),
+            wiki = true,
+            diary = false
         },
         {
-          name = "blog",
-          path = os.getenv("BLOG_DIR")
+            name = "blog",
+            path = os.getenv("BLOG_DIR"),
+            wiki = true,
+            diary = false
         },
         {
             name = "diary",
-            path = os.getenv("DIARY_DIR")
+            path = os.getenv("DIARY_DIR"),
+            diary = true,
+            wiki = false
         }
     },
     keys = {
         {'<leader>kw', function() require("kiwi").open_wiki_index {} end, desc = "Open VimWiki index"},
         {'<leader>kd', function() require("kiwi").open_diary_index {} end, desc = "Open diary index"},
         {'<leader>kn', function() require("kiwi").open_diary_new {} end, desc = "New diary entry"},
-        -- {'<leader-x>', require("kiwi").todo.toggle, desc = "Toggle VimWiki todo"},
+        -- {'\\x', require("kiwi").todo.toggle, desc = "Toggle VimWiki todo"},
     },
 }
 
