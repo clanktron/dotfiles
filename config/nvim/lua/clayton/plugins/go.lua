c2be85306e17659d89973c551e1816da8fc0b851
clayton <crdcastro5@gmail.com>
2023-08-28 00:22:27 -0700

    fix(go.nvim): use updated author recommendation for lazy config

--- config/nvim/lua/clayton/plugins/go.lua
+++ config/nvim/lua/clayton/plugins/go.lua
@@ -1,15 +1,13 @@
 return {
     'ray-x/go.nvim',
-    ft = "go",
+    ft = {"go", 'gomod'},
     lazy = true,
-    config = function()
-        local format_sync_grp = vim.api.nvim_create_augroup("GoFormat", {})
-        vim.api.nvim_create_autocmd("BufWritePre", {
-          pattern = "*.go",
-          callback = function()
-           require('go.format').goimport()
-          end,
-          group = format_sync_grp,
-        })
-    end,
+    dependencies = {
+      "neovim/nvim-lspconfig",
+      "nvim-treesitter/nvim-treesitter",
+    },
+    event = {"CmdlineEnter"},
+    build = function ()
+        require("go.install").update_all_sync()
+    end
 }
