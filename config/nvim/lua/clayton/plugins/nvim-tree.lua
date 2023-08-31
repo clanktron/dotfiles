cd9b18fcb2cdaffa3eb285e928f23e80365bf788
clayton <crdcastro5@gmail.com>
2023-08-30 20:29:14 -0700

    feat(nvim): re-enable netrw
    - gx command for links is way too useful
    - shouldn't break anything with nvim-tree

--- config/nvim/lua/clayton/plugins/nvim-tree.lua
+++ config/nvim/lua/clayton/plugins/nvim-tree.lua
@@ -16,8 +16,8 @@ return {
         },
         init = function ()
             -- disable netrw at the very start of your init.lua (strongly advised)
-            vim.g.loaded_netrw = 1
-            vim.g.loaded_netrwPlugin = 1
+            -- vim.g.loaded_netrw = 1
+            -- vim.g.loaded_netrwPlugin = 1
         end,
     },
 }
