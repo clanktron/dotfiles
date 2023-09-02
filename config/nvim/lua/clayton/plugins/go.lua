bff5847f5652c3e3f661fd506b6ed135a93bf46c
clayton <crdcastro5@gmail.com>
2023-09-02 01:09:14 -0700

    feat(go.nvim): gofmt and goimport autocmd

--- config/nvim/lua/clayton/plugins/go.lua
+++ config/nvim/lua/clayton/plugins/go.lua
@@ -9,5 +9,17 @@ return {
     event = {"CmdlineEnter"},
     build = function ()
         require("go.install").update_all_sync()
+    end,
+    config = function ()
+        require('go').setup()
+        -- Run gofmt + goimport on save
+        local format_sync_grp = vim.api.nvim_create_augroup("GoImport", {})
+        vim.api.nvim_create_autocmd("BufWritePre", {
+          pattern = "*.go",
+          callback = function()
+           require('go.format').goimport()
+          end,
+          group = format_sync_grp,
+        })
     end
 }
