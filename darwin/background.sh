76ab1f5e7857cf476233a333add09f7e012e9f54
Clayton Castro <clayton@wifi-131-179-1-214.host.ucla.edu>
2023-09-28 15:42:54 -0700

    feat(darwin): profile pic setup + asset

--- darwin/background.sh
+++ darwin/background.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 DOTFILES_DIR=$(git rev-parse --show-toplevel)
+# set background picture
 osascript -e "tell application \"Finder\" to set desktop picture to POSIX file \"$DOTFILES_DIR/assets/black-grey-mountain.png\""
+# set profile picture
+dscl . create /Users/"$USER" Picture "$DOTFILES_DIR/assets/profile-garfield.png"
