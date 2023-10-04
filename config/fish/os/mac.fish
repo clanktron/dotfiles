7ae21180b441067d36f395bbf52e935e5d35e418
clayton <crdcastro5@gmail.com>
2023-10-04 11:53:59 -0700

    feat(darwin): nvim symlink convenience script, mac poweroff alias

--- config/fish/os/mac.fish
+++ config/fish/os/mac.fish
@@ -16,3 +16,4 @@ set -ax PATH /Applications/Wireshark.app/Contents/MacOS
 alias sleep1 'sudo pmset disablesleep -1'
 alias sleep0 'sudo pmset disablesleep -0'
 alias dns 'sudo dscacheutil -flushcache;sudo killall -HUP mDNSResponder'
+alias poweroff 'sudo shutdown -h now'
