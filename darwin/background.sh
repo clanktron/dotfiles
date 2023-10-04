8f4251eed4008ccd573d144b4480e1191654d8db
Clayton Castro <clayton@mb-air.local>
2023-10-04 01:42:45 -0700

    feat(darwin): disable desktop icons

--- darwin/background.sh
+++ darwin/background.sh
@@ -5,5 +5,7 @@ WALLPAPER="${WALLPAPER:-$ASSETS_DIR/black-grey-mountain.png}"
 PROFILE_PICTURE="${PROFILE_PICTURE:-$ASSETS_DIR/profile-rose.jpeg}"
 # set background picture
 osascript -e "tell application \"Finder\" to set desktop picture to POSIX file \"$WALLPAPER\""
+# disable background icons
+defaults write com.apple.finder CreateDesktop false
 # set profile picture
 sudo dscl . create /Users/"$USER" Picture "$PROFILE_PICTURE"
