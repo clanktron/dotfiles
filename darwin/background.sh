0a20ab557df5b89b903bdb5341d2261e29fb8ce8
clayton <crdcastro5@gmail.com>
2023-10-03 23:50:54 -0700

    fix: refine darwin photo automation

--- darwin/background.sh
+++ darwin/background.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 DOTFILES_DIR=$(git rev-parse --show-toplevel)
+ASSETS_DIR="${ASSETS_DIR:-$DOTFILES_DIR/assets}"
+WALLPAPER="${WALLPAPER:-$ASSETS_DIR/black-grey-mountain.png}"
+PROFILE_PICTURE="${PROFILE_PICTURE:-$ASSETS_DIR/profile-rose.jpeg}"
 # set background picture
-osascript -e "tell application \"Finder\" to set desktop picture to POSIX file \"$DOTFILES_DIR/assets/black-grey-mountain.png\""
+osascript -e "tell application \"Finder\" to set desktop picture to POSIX file \"$WALLPAPER\""
 # set profile picture
-dscl . create /Users/"$USER" Picture "$DOTFILES_DIR/assets/profile-garfield.png"
+sudo dscl . create /Users/"$USER" Picture "$PROFILE_PICTURE"
