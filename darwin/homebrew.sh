61ac142bc7907ef73e784f29d4a253c0c987f01f
clayton <crdcastro5@gmail.com>
2023-10-04 22:11:25 -0700

    feat(darwin): homebrew script now installs user specified bundle

--- darwin/homebrew.sh
+++ darwin/homebrew.sh
@@ -1,3 +1,27 @@
 #!/bin/sh
 # Install homebrew
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+echo "Checking if Homebrew is installed..."
+if ! brew --version > /dev/null; then
+    echo "Homebrew isn't installed, installing now..."
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+else
+    echo "Homebrew is already installed, proceeding..."
+fi
+# Select profile
+echo "Which brew bundle would you like to install? (mbp, mb-air)"
+read -r profile
+case "$profile" in
+  mbp | complete)
+    echo "Setting brewfile bundle to mbp profile..."
+    HOMEBREW_BREWFILE=../config/brewfile/Brewfile.mbp
+    ;;
+  mb-air | minimal)
+    echo "Setting brewfile bundle to mb-air profile..."
+    HOMEBREW_BREWFILE=../config/brewfile/Brewfile.mb-air
+    ;;
+  *)
+    echo "Invalid choice. Please choose mbp or mb-air."
+    ;;
+esac
+# brew bundle install
+echo "installing $HOMEBREW_BREWFILE"
