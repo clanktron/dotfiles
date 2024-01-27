b52d8accf66638f1241d3d47d980957a338d4905
clayton <crdcastro5@gmail.com>
2024-01-26 20:47:30 -0800

    fix(fish): ensure no failure on multiple ssh-agents

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -74,7 +74,7 @@ switch (uname)
 end
 
 # Ensure ssh agent
-if test -z (pgrep ssh-agent)
+if test -z (pgrep ssh-agent | string collect)
   eval (ssh-agent -c)
   set -Ux SSH_AUTH_SOCK $SSH_AUTH_SOCK
   set -Ux SSH_AGENT_PID $SSH_AGENT_PID
