6ae8ba63b5c5183c355d0e6c1262e1cf7cc04aea
clayton <hello@claytonc.dev>
2024-05-05 03:48:55 -0700

    nvim-tree config

--- config/nvim/lua/clayton/plugins/minimal/nvim-tree.lua
+++ config/nvim/lua/clayton/plugins/minimal/nvim-tree.lua
@@ -8,16 +8,23 @@ return {
         opts = {
             view = {
                 width = 35,
+                float = {
+                    enable = false,
+                    open_win_config = {
+                        width = 45,
+                    },
+                }
+            },
+            renderer = {
+              add_trailing = false,
+              group_empty = false,
+              full_name = false,
+              root_folder_label = ":~:s?$?",
             }
         },
         keys = {
             -- { "<leader>e", "<cmd>NvimTreeToggle<cr> <Cmd>set relativenumber<CR>", desc = "Open directory with nvim-tree"},
             { "<leader>e", function() vim.cmd.NvimTreeToggle() vim.opt.relativenumber = true end, desc = "Open directory with nvim-tree"},
         },
-        init = function ()
-            -- disable netrw at the very start of your init.lua (strongly advised)
-            -- vim.g.loaded_netrw = 1
-            -- vim.g.loaded_netrwPlugin = 1
-        end,
     },
 }
