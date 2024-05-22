2b9af35caa5e0c7d93676c3f0f520e2766680073
clayton <crdcastro5@gmail.com>
2024-05-22 13:22:24 -0700

    fish: check for bw cli before fetching secrets

--- config/fish/secrets.fish
+++ config/fish/secrets.fish
@@ -1,3 +1,5 @@
-set -q GITHUB_TOKEN; or echo "fetching GITHUB_TOKEN from bitwarden..." && set -gx GITHUB_TOKEN (bw get item github | jq -j '.fields[] | select(.name == "mbp-token") | .value')
-set -q PGP_PASSWORD; or echo "fetching PGP_PASSWORD from bitwarden..." && set -gx PGP_PASSWORD (bw get item 'gpg key' | jq -j '.fields[] | select(.name == "key") | .value')
-set -q RESTIC_PASSWORD; or echo "fetching RESTIC_PASSWORD from bitwarden..." && set -gx RESTIC_PASSWORD (bw get item restic | jq -j '.login.password')
+if command -q bw
+    set -q GITHUB_TOKEN; or echo "fetching GITHUB_TOKEN from bitwarden..." && set -gx GITHUB_TOKEN (bw get item github | jq -j '.fields[] | select(.name == "mbp-token") | .value')
+    set -q PGP_PASSWORD; or echo "fetching PGP_PASSWORD from bitwarden..." && set -gx PGP_PASSWORD (bw get item 'gpg key' | jq -j '.fields[] | select(.name == "key") | .value')
+    set -q RESTIC_PASSWORD; or echo "fetching RESTIC_PASSWORD from bitwarden..." && set -gx RESTIC_PASSWORD (bw get item restic | jq -j '.login.password')
+end
