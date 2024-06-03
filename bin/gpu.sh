f5f37201c5c28b1ef267a597b8ae83d7b1ef2295
clayton <crdcastro5@gmail.com>
2024-06-03 13:25:40 -0700

    waybar: adjust gpu custom module

--- bin/gpu.sh
+++ bin/gpu.sh
@@ -3,4 +3,4 @@ gpu="${1:-/sys/class/hwmon/hwmon3}"
 load=$(cat "$gpu"/device/gpu_busy_percent)
 temp=$(cat "$gpu"/temp1_input)
 temp=$(("$temp" / 1000))
-printf " %s%% %s°C" "$load" "$temp"
+printf "  %s°C   %s%% " "$temp" "$load"
