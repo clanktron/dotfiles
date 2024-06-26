7c03b764b1a0f3a0969790fd3e84270762c8f7b0
clayton <hello@claytonc.dev>
2024-06-25 18:15:44 -0700

    pgp pass convenience func

--- config/fish/functions/pgp.fish
+++ config/fish/functions/pgp.fish
@@ -1,4 +1,12 @@
 function pgp
-    echo -n "$PGP_PASSWORD" | pbcopy
+    switch (uname)
+      case Darwin
+        set copy pbcopy
+      case Linux
+        set copy wl-copy
+      case '*'
+          echo "AAAAAAAAAAAAAHHHHHHHHHHHHHHH"
+    end
+    echo -n "$PGP_PASSWORD" | "$copy"
     echo "pgp key password copied to clipboard..."
 end
