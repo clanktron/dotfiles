72c7042100b2765f139f987c005081618194d3d3
clayton <hello@claytonc.dev>
2024-08-10 03:17:16 -0700

    add initial nix configs

---
+++ nix/default.nix
@@ -0,0 +1,31 @@
+{ pkgs ? import <nixpkgs> {} }:
+
+pkgs.buildEnv {
+    name = "personal";
+    paths = with pkgs; [
+        neovim
+        tree
+        tmux
+        kubectl
+        bat
+        zoxide
+        ripgrep
+        k9s
+        nerdfonts
+        font-awesome
+        lazygit
+        gnumake
+        gcc
+        gnupg
+        alacritty
+        mpv
+        bitwarden-cli
+        bws
+
+        devspace
+        azure-cli
+        cosign
+        syft
+        gh
+    ];
+}
