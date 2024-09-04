e990548fa34848c19341f552f388bc0293f21351
clayton <hello@claytonc.dev>
2024-09-04 00:34:30 -0700

    nix: update configuration.nix

--- nix/configuration.nix
+++ nix/configuration.nix
@@ -1,7 +1,3 @@
-# Edit this configuration file to define what should be installed on
-# your system.  Help is available in the configuration.nix(5) man page
-# and in the NixOS manual (accessible by running ‘nixos-help’).
-
 { config, pkgs, ... }:
 
 {
@@ -14,22 +10,14 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
-  networking.hostName = "nixos"; # Define your hostname.
+  networking.hostName = "nixos";
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
-  # Enable networking
   networking.networkmanager.enable = true;
 
-  # Set your time zone.
   time.timeZone = "America/Los_Angeles";
 
   # Select internationalisation properties.
   i18n.defaultLocale = "en_US.UTF-8";
-
   i18n.extraLocaleSettings = {
     LC_ADDRESS = "en_US.UTF-8";
     LC_IDENTIFICATION = "en_US.UTF-8";
@@ -42,14 +30,35 @@
     LC_TIME = "en_US.UTF-8";
   };
 
-  # Enable the X11 windowing system.
   services.xserver.enable = true;
-
-  # Enable the GNOME Desktop Environment.
   services.xserver.displayManager.gdm.enable = true;
   services.xserver.desktopManager.gnome.enable = true;
 
-  # Configure keymap in X11
+  xdg = {
+    sounds.enable = true;
+    portal = {
+      enable = true;
+      # needed if gnome isn't installed
+      # extraPortals = with pkgs; [
+      # 	xdg-desktop-portal-gtk
+      # ];
+      xdgOpenUsePortal = true;
+      wlr.enable = true;
+      config = {
+        common.default = ["gtk"];
+	sway = {
+	  default = [ "wlr" ];
+	};
+	swayfx = {
+	  default = [ "wlr" ];
+	};
+	Hyprland = {
+	  default = [ "hyprland" ];
+	};
+      };
+    };
+  };
+
   services.xserver.xkb = {
     layout = "us";
     variant = "";
@@ -57,15 +66,33 @@
 
   services.flatpak.enable = true;
   services.gnome.gnome-keyring.enable = true;
-  # Enable CUPS to print documents.
-  services.printing.enable = true;
+  services.gnome.sushi.enable = true;
+  services.gnome.gnome-settings-daemon.enable = true;
+  services.gnome.gnome-online-accounts.enable = true;
+  services.printing.enable = true; # Enable CUPS to print documents.
 
-  hardware.opengl = {
-    enable = true;
-    driSupport = true;
-    driSupport32Bit = true;
-    # added for gpu-screen-recorder-gtk
-    extraPackages = with pkgs; [
+
+  hardware = {
+    graphics.enable = true;
+    enableAllFirmware = true;
+    bluetooth.enable = true;
+    pulseaudio.enable = false;
+    firmware = with pkgs; [
+      rtl8761b-firmware
+    ];
+    cpu = {
+      intel.sgx.provision.enable = true;
+    };
+    intel-gpu-tools.enable = true;
+    amdgpu = {
+      opencl.enable = true;
+      amdvlk.enable = true;
+    };
+    acpilight.enable = true;
+    uinput.enable = true;
+    i2c.enable = true;
+    steam-hardware.enable = true;
+    graphics.extraPackages = with pkgs; [
       intel-media-driver # LIBVA_DRIVER_NAME=iHD
       vaapiIntel         # LIBVA_DRIVER_NAME=i965 (older but works better for Firefox/Chromium)
       vaapiVdpau
@@ -73,80 +100,88 @@
     ];
   };
 
-  hardware.steam-hardware.enable = true;
-  hardware.pulseaudio.enable = false;
+  virtualisation = {
+    lxd.enable = true;
+    lxd.agent.enable = true;
+    lxc.enable = true;
+    kvmgt.enable = true;
+    containerd.enable = true;
+  };
+
   security.rtkit.enable = true;
   services.pipewire = {
     enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
     pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
-
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
+    # extraConfig.pipewire."10-clock-rate" = {
+    #   "context.properties" = {
+    #     "default.clock.rate"          = 48000;
+    #     "default.clock.allowed-rates" = [ 48000 ];
+    #     "default.clock.quantum"       = 2048;
+    #     "default.clock.min-quantum"   = 1024;
+    #   };
+    # };
   };
 
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
-
-  # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.clayton = {
     isNormalUser = true;
     description = "Clayton";
     extraGroups = [ "networkmanager" "wheel" ];
     shell = pkgs.fish;
     packages = with pkgs; [
-    #  thunderbird
     ];
   };
 
-  xdg = {
-    portal = {
-      enable = true;
-      wlr.enable = true;
-      config = {
-        common.default = ["gtk" "wlr"];
-      };
-    };
-  };
-
+  nixpkgs.config.allowUnfree = true;
   programs.firefox.enable = true;
   programs.fish.enable = true;
   programs.sway = {
     enable = true;
+    # package = pkgs.swayfx;
     wrapperFeatures.gtk = true;
+    extraPackages = with pkgs; [
+      waybar
+      grim
+      slurp
+      wl-clipboard
+      wf-recorder
+      wl-screenrec
+      wdisplays
+      swaylock
+      chayang
+      swayidle
+      swaynotificationcenter
+      swayosd
+      wlogout
+      wofi
+      swww
+      nwg-dock
+      nwg-displays
+      wtype
+      brightnessctl
+      playerctl
+      blueman
+      networkmanagerapplet
+      gnome-calendar
+      gnome-disk-utility
+      gnome.gnome-control-center
+    ];
   };
-
-  # Allow unfree packages
-  nixpkgs.config.allowUnfree = true;
-
+  programs.hyprland = {
+    enable = true;
+  };
+  programs.hyprlock.enable = true;
   environment.systemPackages = with pkgs; [
+    hypridle
+    alacritty
     vim
     git
-    containerd
     nerdctl
     go
-    wl-clipboard
-    flatpak
-    gnome.gnome-software
-    swaylock
-    swayidle
-    swaynotificationcenter
-    waybar
-    wlogout
-    wofi
-    swww
-    grim
-    slurp
+    lm_sensors
+    nvtopPackages.full
     ddcutil
-    brightnessctl
-    playerctl
-    hyprlock
-    blueman
-    pavucontrol
+    buildkit
+    pinentry-gnome3
   ];
 
   # Some programs need SUID wrappers, can be configured further or are
@@ -158,9 +193,8 @@
   # };
 
   # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
+  services.openssh.enable = true;
+  services.tailscale.enable = true;
 
   # Open ports in the firewall.
   # networking.firewall.allowedTCPPorts = [ ... ];
