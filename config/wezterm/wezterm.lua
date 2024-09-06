4c25aaa2cf9f13bff8bec136873675621ffeacd6
clayton <hello@claytonc.dev>
2024-09-05 21:18:07 -0700

    wezterm: auto dark/light switch with OS, fonts, etc

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -13,26 +13,41 @@ end
 -- This is where you actually apply your config choices
 
 -- For example, changing the color scheme:
-config.color_scheme = 'Tokyo Night Storm'
+function get_appearance()
+  if wezterm.gui then
+    return wezterm.gui.get_appearance()
+  end
+  return 'Dark'
+end
+
+function scheme_for_appearance(appearance)
+  if appearance:find 'Dark' then
+    return 'Tokyo Night Storm'
+  else
+    return 'Github Light (Gogh)'
+  end
+end
+
+config.color_scheme = scheme_for_appearance(get_appearance())
 config.freetype_load_flags = 'NO_HINTING'
 config.window_background_opacity = 0.95
 config.font_size = 16
-config.font = wezterm.font 'JetBrains Mono'
+config.font = wezterm.font 'SauceCodePro Nerd Font'
 config.font_rules = {
 	{
 		intensity = "Bold",
 		italic = false,
-		font = wezterm.font("JetBrains Mono", { weight = "Bold", stretch = "Normal", style = "Normal" }),
+		font = wezterm.font("SauceCodePro Nerd Font", { weight = "Bold", stretch = "Normal", style = "Normal" }),
 	},
 	{
 		intensity = "Bold",
 		italic = true,
-		font = wezterm.font("JetBrains Mono", { weight = "Bold", stretch = "Normal", style = "Italic" }),
+		font = wezterm.font("SauceCodePro Nerd Font", { weight = "Bold", stretch = "Normal", style = "Italic" }),
 	},
 }
 config.window_decorations = "TITLE|RESIZE"
 config.enable_tab_bar = false
-config.enable_wayland = false
+config.enable_wayland = true
 config.window_close_confirmation = 'NeverPrompt'
 config.keys = {
   {
