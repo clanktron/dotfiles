94b7f3c5411a50a1213f038f5fc500a8f5ba9c12
clayton <hello@claytonc.dev>
2025-04-03 23:40:59 -0700

    nvim: only require c stuff

--- config/nvim/lua/plugins/base/treesitter.lua
+++ config/nvim/lua/plugins/base/treesitter.lua
@@ -3,7 +3,10 @@ return {
     build = ':TSUpdate',
     config = function()
         require('nvim-treesitter.configs').setup {
-            ensure_installed = "all",
+            ensure_installed = {
+                "c",
+                "cpp"
+            },
             sync_install = false,
             auto_install = true,
             highlight = {
