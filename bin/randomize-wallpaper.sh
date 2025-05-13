c0cc28ca7dca124add7c2249caf6d32d00fcffbc
clayton <hello@claytonc.dev>
2025-05-12 19:01:57 -0700

    set wallpaper system wide

--- bin/randomize-wallpaper.sh
+++ bin/randomize-wallpaper.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 new_wallpaper=$(find /home/clayton/Developer/repos/clanktron/dotfiles/assets/wallpapers/ -type f | shuf -n 1)
-swww img "$new_wallpaper"
+ln -sf $new_wallpaper ~/.config/background
+swww img ~/.config/background
