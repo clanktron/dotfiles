072f03eec31398d78e61227e906cd692afca1877
clayton <hello@claytonc.dev>
2025-05-14 16:04:58 -0700

    fix(nvim): don't call setup during lsp config

--- config/nvim/lua/plugins/lsp/lsp.lua
+++ config/nvim/lua/plugins/lsp/lsp.lua
@@ -122,21 +122,22 @@ return {
             }
             local mason_packages = vim.fn.stdpath("data") .. "/mason/packages"
             local volar_path = mason_packages .. "/vue-language-server/node_modules/@vue/language-server"
-            vim.lsp.config.ts_ls.setup = {
-              -- NOTE: To enable Hybrid Mode, change hybrideMode to true above and uncomment the following filetypes block.
-              -- WARN: THIS MAY CAUSE HIGHLIGHTING ISSUES WITHIN THE TEMPLATE SCOPE WHEN TSSERVER ATTACHES TO VUE FILES
-              -- filetypes = { "typescript", "javascript", "javascriptreact", "typescriptreact", "vue" },
-              init_options = {
-                plugins = {
-                  {
-                    name = "@vue/typescript-plugin",
-                    location = volar_path,
-                    languages = { "vue" },
+            vim.lsp.config.ts_ls = {
+              setup = {
+                -- NOTE: To enable Hybrid Mode, change hybrideMode to true above and uncomment the following filetypes block.
+                -- WARN: THIS MAY CAUSE HIGHLIGHTING ISSUES WITHIN THE TEMPLATE SCOPE WHEN TSSERVER ATTACHES TO VUE FILES
+                -- filetypes = { "typescript", "javascript", "javascriptreact", "typescriptreact", "vue" },
+                init_options = {
+                  plugins = {
+                    {
+                      name = "@vue/typescript-plugin",
+                      location = volar_path,
+                      languages = { "vue" },
+                    },
                   },
                 },
-              },
+              }
             }
-
             mason.setup()
             mason_lspconfig.setup()
         end,
