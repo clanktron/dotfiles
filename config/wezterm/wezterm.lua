049782916e9333890b8991eac509a5d1f9e679ff
clayton <hello@claytonc.dev>
2025-06-12 16:37:01 -0700

    wezterm: sessionizer

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -1,7 +1,4 @@
--- Pull in the wezterm API
 local wezterm = require 'wezterm'
-
--- This table will hold the configuration.
 local config = {}
 
 -- In newer versions of wezterm, use the config_builder which will
@@ -29,116 +26,20 @@ config.color_scheme = scheme_for_appearance(get_appearance())
 config.freetype_load_flags = 'NO_HINTING'
 config.window_background_opacity = 1
 config.font_size = 13
+config.font = wezterm.font 'SauceCodePro Nerd Font'
 config.warn_about_missing_glyphs = false
 config.window_decorations = "TITLE|RESIZE"
 config.enable_tab_bar = false
 config.enable_wayland = true
 config.window_close_confirmation = 'NeverPrompt'
 
-local act = wezterm.action
+local sessionizer = require("sessionizer")
+
 config.keys = {
-    {
-        key = 'w',
-        mods = 'CMD',
-        action = act.Multiple {
-            act.SendKey { key = "a", mods = "CTRL" },
-            act.SendKey { key = "&" },
-        }
-    },
-    {
-        key = 'v',
-        mods = 'CMD',
-        action = act.PasteFrom 'Clipboard'
-    },
-    {
-        key = 'r',
-        mods = 'CMD',
-        action = act.Multiple {
-            act.SendKey { key = "a", mods = "CTRL" },
-            act.SendKey { key = "{" },
-        }
-    },
-    {
-        key = 't',
-        mods = 'CMD',
-        action = act.Multiple {
-            act.SendKey { key = "a", mods = "CTRL" },
-            act.SendKey { key = "c" },
-        }
-    },
-    {
-        key = '1',
-        mods = 'CMD',
-        action = act.Multiple {
-            act.SendKey { key = "a", mods = "CTRL" },
-            act.SendKey { key = "1" },
-        }
-    },
-    {
-        key = '2',
-        mods = 'CMD',
-        action = act.Multiple {
-            act.SendKey { key = "a", mods = "CTRL" },
-            act.SendKey { key = "2" },
-        }
-    },
-    {
-        key = '3',
-        mods = 'CMD',
-        action = act.Multiple {
-            act.SendKey { key = "a", mods = "CTRL" },
-            act.SendKey { key = "3" },
-        }
-    },
-    {
-        key = '4',
-        mods = 'CMD',
-        action = act.Multiple {
-            act.SendKey { key = "a", mods = "CTRL" },
-            act.SendKey { key = "4" },
-        }
-    },
-    {
-        key = '5',
-        mods = 'CMD',
-        action = act.Multiple {
-            act.SendKey { key = "a", mods = "CTRL" },
-            act.SendKey { key = "5" },
-        }
-    },
-    {
-        key = '6',
-        mods = 'CMD',
-        action = act.Multiple {
-            act.SendKey { key = "a", mods = "CTRL" },
-            act.SendKey { key = "6" },
-        }
-    },
-    {
-        key = '7',
-        mods = 'CMD',
-        action = act.Multiple {
-            act.SendKey { key = "a", mods = "CTRL" },
-            act.SendKey { key = "7" },
-        }
-    },
-    {
-        key = '8',
-        mods = 'CMD',
-        action = act.Multiple {
-            act.SendKey { key = "a", mods = "CTRL" },
-            act.SendKey { key = "8" },
-        }
-    },
-    {
-        key = '9',
-        mods = 'CMD',
-        action = act.Multiple {
-            act.SendKey { key = "a", mods = "CTRL" },
-            act.SendKey { key = "9" },
-        }
-    },
+    { key = "f", mods = "SUPER", action = wezterm.action_callback(sessionizer.toggle) },
+    { key = 'f', mods = 'SUPER|SHIFT', action = wezterm.action.ShowLauncherArgs { flags = 'FUZZY|WORKSPACES' } },
+    { key = '[', mods = 'SUPER', action = wezterm.action.SwitchWorkspaceRelative(1) },
+    { key = ']', mods = 'SUPER', action = wezterm.action.SwitchWorkspaceRelative(-1) },
 }
 
--- and finally, return the configuration to wezterm
 return config
