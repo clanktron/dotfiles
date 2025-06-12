45d6f9beb6ab50bfd91f1370c1129c3c6a88a7f6
clayton <hello@claytonc.dev>
2025-06-11 18:28:37 -0700

    nvim: add remote plugin

---
+++ config/nvim/lua/plugins/remote.lua
@@ -0,0 +1,9 @@
+return {
+   "hmk114/remote-nvim.nvim",
+   dependencies = {
+       "nvim-lua/plenary.nvim", -- For standard functions
+       "MunifTanjim/nui.nvim", -- To build the plugin UI
+       "nvim-telescope/telescope.nvim", -- For picking b/w different remote methods
+   },
+   config = true,
+}
