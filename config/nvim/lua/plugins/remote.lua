07b25fb1e7ca2f87f634c299311e21d3d3e9d61d
clayton <hello@claytonc.dev>
2025-06-12 19:27:42 -0700

    nvim: add preliminary remote opts

--- config/nvim/lua/plugins/remote.lua
+++ config/nvim/lua/plugins/remote.lua
@@ -5,5 +5,13 @@ return {
        "MunifTanjim/nui.nvim", -- To build the plugin UI
        "nvim-telescope/telescope.nvim", -- For picking b/w different remote methods
    },
-   config = true,
+   opts = {
+      client_callback = function(port, _)
+        local exit_code = vim.fn.jobstart({"open", "-na", "ghostty", "--args", "-e", ("nvim --remote-ui --server localhost:%s"):format(port)}, { detach = true })
+        if exit_code ~= 0 then
+          vim.notify(("Local client failed with exit code %s"):format(exit_code), vim.log.levels.ERROR)
+        end
+      end,
+   },
+   config = true
 }
